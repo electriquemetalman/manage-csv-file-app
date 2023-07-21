a96ea935c28ded3956311ff37b061c77121379f5
Stiving Brownd <stivingtatga@gmail.com>
2023-07-21 02:32:38 +0100

    create migration, controller, model, route api, and set up the database in .env file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\competitionController;
+use App\Http\Controllers\visitorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('apiv1')->group(function () {
+    Route::apiResource('competition', competitionController::class);
+    Route::apiResource('visitor', visitorController::class);
 });
