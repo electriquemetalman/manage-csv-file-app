856cff89b749df5bdbd999234697cc91665dced8
Stiving Brownd <stivingtatga@gmail.com>
2023-08-08 23:04:57 +0100

    creud on competition table

--- app/Http/Controllers/competitionController.php
+++ app/Http/Controllers/competitionController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\competitionModel;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Http\Requests\competitionRequest;
+use App\Models\competition;
+use Exception;
 
 class competitionController extends Controller
 {
@@ -13,37 +17,105 @@ class competitionController extends Controller
     public function index()
     {
         //
+        try {
+            $competitions = Competition::all();
+            return response()->json($competitions, 200);
+        } catch (ModelNotFoundException $exception) {
+            return $exception->getMessage();
+        } catch (Exception $exception) {
+            return $exception->getMessage();
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(competitionRequest $request)
     {
         //
+        $validated = $request->validated();
+
+        $csvFileName = time() . '.' . $validated['ref_file']->extension();
+        $validated['ref_file']->move(public_path('ref_csvFiles/'), $csvFileName);
+
+        $competition = Competition::create([
+            'title' => $validated['title'],
+            'litel_description' => $validated['litel_description'],
+            'long_description' => $validated['long_description'],
+            'evaluation_text' => $validated['evaluation_text'],
+            'ref_file' => $csvFileName
+        ]);
+        return response()->json($competition, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(competitionModel $competitionModel)
+    public function show($id)
     {
         //
+        try {
+            $competition = Competition::findorfail($id);
+            return response()->json($competition, 201);
+        } catch (Exception $exception) {
+            return $exception->getMessage();
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, competitionModel $competitionModel)
+    public function update(Request $request, $id)
     {
         //
+
+        $competition = Competition::findorfail($id);
+
+        $csvFile = $request->ref_file;
+
+        try {
+            if (isset($csvFile) === true) {
+
+                $csvFileName = time() . '.' . $csvFile->extension();
+                $csvFile->move(public_path('ref_csvFiles/'), $csvFileName);
+
+                $competition->update([
+                    'title' => $request->title,
+                    'litel_description' => $request->litel_description,
+                    'long_description' => $request->long_description,
+                    'evaluation_text' => $request->evaluation_text,
+                    'ref_file' => $csvFileName
+                ]);
+            } else {
+                $competition->update([
+                    'title' => $request->title,
+                    'litel_description' => $request->litel_description,
+                    'long_description' => $request->long_description,
+                    'evaluation_text' => $request->evaluation_text
+                ]);
+            }
+            return response()->json([
+                'success' => true,
+                'message' => 'Competition updated successfully!',
+                'updated_data' => $competition
+            ], 200);
+        } catch (Exception $exception) {
+            return $exception->getMessage();
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(competitionModel $competitionModel)
+    public function destroy($id)
     {
         //
+        try {
+            $competition = Competition::findorfail($id);
+            $competition->delete();
+            return response()->json("delete succesfull", 202);
+        } catch (Exception $exception) {
+            return $exception->getMessage();
+        }
     }
 }
---
+++ app/Http/Requests/competitionRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class competitionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'title' => 'required|min:4',
+            'litel_description' => 'required|min:10',
+            'long_description' => 'required|min:30',
+            'evaluation_text' => 'required|min:10',
+            'ref_file' => 'required',
+        ];
+    }
+}
